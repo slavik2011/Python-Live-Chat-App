d4d179879fa55368dedaeb2f27eaa621e3561d88
slavik2011 <89517629+slavik2011@users.noreply.github.com>
2024-11-19 12:10:06 -0800

    adding conf

---
+++ configuration.py
@@ -0,0 +1,20 @@
+# This file contains configurations that are save to change and will not harm your program at all. To change it you have to fork this repo and deploy your fork.
+
+# This configuration contains different types of settings, always read descriptions and comments before changing any parameter.
+
+class rooms:
+  pass
+
+class security:
+  pass
+
+class storage:
+  pass
+
+class colors:
+  pass
+
+# CONFIGURATION - ROOMS
+rooms.max_members = 0 # set 0 to make it infinite
+rooms.max_symbols = 0 # set 0 to make it infinite
+rooms.message_delay = 0 # set 0 to remove delay
