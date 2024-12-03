f30749d353223ada65146ac2145f39370126e5ed
slavik2011 <89517629+slavik2011@users.noreply.github.com>
2024-12-03 08:43:04 -0800

    typo in word safe

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-# This file contains configurations that are save to change and will not harm your program at all. To change it you have to fork this repo and deploy your fork.
+# This file contains configurations that are safe to change and will not harm your program at all. To change it you have to fork this repo and deploy your fork.
 
 # This configuration contains different types of settings, always read descriptions and comments before changing any parameter.
 
